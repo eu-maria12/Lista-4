248f93b8a2eeb4fd8870e1f6c1c856852dd20702
eu-maria12 <mariafernandacvo12@gmail.com>
2025-10-14 21:17:45 -0300

    Update dez.java

--- dez.java
+++ dez.java
@@ -5,14 +5,31 @@ public class dez{
 
         Scanner input= new Scanner(System.in);
         int numero;
-
+        int total = 0;
+        int pares = 0; 
+        int impares = 0;
+        int multiplosDe5 = 0;
+    
         System.out.println("Digite algum número de 0 a 10:");
         numero = input.nextInt();
 
         while(numero != 0) {
-            System.out.println("Você digitou: " + numero + ". Digite outro número: ");
-            numero= input.nextInt();
+                total++;
+            if(numero % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+            if (numero % 5 == 0){
+                    multiplosDe5++;
+            }
+             System.out.println("Digite outro número de 0 a 10:");
+             numero = input.nextInt();
         }
-            System.out.println("Você digitou o número 0. Programa encerrado.");
-        }
+        System.out.println("Total de números digitados: " + total);
+        System.out.println("Números pares: " + pares);
+        System.out.println("Números ímpares: " + impares);
+        System.out.println("Múltiplos de 5: " + multiplosDe5);
+        System.out.println("Programa encerrado!")
     }
+}
